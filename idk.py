c1e7bbd3f74d731c5ada54b89fc85790d3b71ec8
Rodrigo Ortega <91147909+RodrigoOrtega1@users.noreply.github.com>
2025-02-22 17:09:12 -0600

    nueva version

--- idk.py
+++ idk.py
@@ -8,24 +8,54 @@
 # ngrok http http://localhost:5000
 # ngrok http 5000 --url verbally-intent-boar.ngrok-free.app -> en otra terminal una vez la aplicacion esta corriendo, cerrar esta terminal cierra ngrok
 from flask import Flask, request, jsonify
+import requests
+
+valor = 0
+next_node = "http://localhost:5002"
+name = "Rodrigo 1"
 
 stored_data = {}
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/data', methods=['POST'])
+def received_data():
+    global valor
+    data = request.get_json()
 
-@app.route('/post', methods=['POST'])
-def handle_post():
-    global stored_data
-    input_json = request.get_json(force=True)
-    print ("Data from client: ", input_json)
-    stored_data = input_json
-    dictToReturn = {"answer" : "Recibido"}
-    return jsonify(dictToReturn)
+    if "valor" in data:
+        valor = data["valor"] + 1
+        print(f"Servidor: {name} recibe el valor {valor}")
+        if valor >= 50:
+            return jsonify({"message":f"Se ha llegado al valor {valor}, terminando la ejecucion"}), 200
+        else:
+            send_data(next_node)
+            return jsonify({"message":f"Dato recibido, enviando al siguiente servidor"}), 200
+    return jsonify({"message":"Error al recibir el valor"}, 400)
+
+def send_data(next_node):
+    global valor
+
+    payload = {
+        "valor": valor,
+        "name": name
+    }
     
-@app.route('/get', methods=['GET'])
-def handle_get():
-    return jsonify(stored_data)
+    try:
+        response = requests.post(next_node + "/data", json=payload)
+        if response.status_code == 200:
+            print(f"El servidor {name} ha enviado el valor: {valor} al servidor {next_node}")
+        else:
+            print(f"Error al enviar el dato al servidor {next_node}")
+    except Exception as e:
+        print(f"Error al enviar el valor al servidor {next_node}")
+
+@app.route('/start', methods=['POST'])
+def start():
+    global valor
+    print(f"{name} - empezara la ejecucion")
+    send_data(next_node)
+    return jsonify({"message":"ok"}, 200)
+
+if __name__ == '__main__':
+    app.run(port=5001)
